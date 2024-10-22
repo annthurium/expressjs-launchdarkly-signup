418284cc7e7c69ae04184b231b0cd0707728ff13
Tilde <tilde@thuryism.net>
2024-10-22 14:48:28 -0700

    add LD flag evaluation + metrics tracking

--- index.js
+++ index.js
@@ -10,35 +10,57 @@ const app = express();
 app.use(serveStatic(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+let context = {
+  kind: "user",
+  anonymous: true,
+  key: "anonymous-123",
+};
+
+app.get("/", async (req, res) => {
+  // Evaluate LaunchDarkly flag
+  const showSeasonalStyling = await ldClient.variation(
+    "show-seasonal-styling",
+    context,
+    false
+  );
+
+  let url;
+  if (showSeasonalStyling === true) {
+    url = "signup-2.html";
+  } else {
+    url = "signup.html";
+  }
+  res.redirect(url);
 });
 
 // Initialize the LaunchDarkly client
 const ldClient = LaunchDarkly.init(process.env.LAUNCHDARKLY_SDK_KEY);
 
-app.post("/login", (req, res) => {
-  // Insert your authentication logic here
-  console.log(req.body);
-  let email = req.body.email;
-  console.log(email);
-  res.send(`email: ${email}`);
+app.post("/login", async (req, res) => {
+  await ldClient.track("signup-conversion", context, 1);
+
+  res.send(
+    `Thanks for signing up! Look for the confirmation email in your inbox: ${req.body.email}`
+  );
+  // insert your actual authentication logic here
 });
 
-// Wait for the client to be ready before starting the server
-ldClient.waitForInitialization().then(() => {
+// Add the waitForInitialization function to ensure the client is ready before starting the server
+const timeoutInSeconds = 5;
+ldClient.waitForInitialization({ timeout: timeoutInSeconds }).then(() => {
   const port = 3000;
   const server = app.listen(port, function (err) {
     if (err) console.log("Error in server setup");
     console.log(`Server listening on http://localhost:${port}`);
+  });
+});
 
-    // Evaluate a feature flag
-    // ldClient.variation("your-feature-flag-key", { key: "user-key" }, false, (err, flagValue) => {
-    //   if (err) {
-    //     console.log("Error evaluating feature flag:", err);
-    //   } else {
-    //     console.log("Feature flag value:", flagValue);
-    //   }
-    // });
+// Add the following new function to gracefully close the connection to the LaunchDarkly server.
+process.on("SIGTERM", () => {
+  debug("SIGTERM signal received: closing HTTP server");
+  ld.close();
+  server.close(() => {
+    debug("HTTP server closed");
+    ldClient.close();
   });
 });
